009580adaf7aa5b7adfa9f2a859aa12b2b04e2aa
Monark Basak <mbasak@uoguelph.ca>
2020-02-26 19:14:19 -0500

    I can uploaded SVG files to my server, need to add file size functionality, call my C functions for parsing the SVG files, and dynamically modify my table based on the files in my upload.

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 
 // C library API
 const ffi = require('ffi-napi');
+const ref = require('ref'); // Used to make pointer variables for functions
 
 // Express App (Routes)
 const express = require("express");
-const app     = express();
-const path    = require("path");
+const app = express();
+const path = require("path");
 const fileUpload = require('express-fileupload');
 
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname+'/uploads')));
+app.use(express.static(path.join(__dirname + '/uploads')));
 
 // Minimization
 const fs = require('fs');
@@ -20,36 +21,41 @@ const JavaScriptObfuscator = require('javascript-obfuscator');
 const portNum = process.argv[2];
 
 // Send HTML at root, do not change
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // Send Style, do not change
-app.get('/style.css',function(req,res){
+app.get('/style.css', function (req, res) {
   //Feel free to change the contents of style.css to prettify your Web app
-  res.sendFile(path.join(__dirname+'/public/style.css'));
+  res.sendFile(path.join(__dirname + '/public/style.css'));
 });
 
 // Send obfuscated JS, do not change
-app.get('/index.js',function(req,res){
-  fs.readFile(path.join(__dirname+'/public/index.js'), 'utf8', function(err, contents) {
-    const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {compact: true, controlFlowFlattening: true});
+app.get('/index.js', function (req, res) {
+  fs.readFile(path.join(__dirname + '/public/index.js'), 'utf8', function (err, contents) {
+    const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {
+      compact: true,
+      controlFlowFlattening: true
+    });
     res.contentType('application/javascript');
     res.send(minimizedContents._obfuscatedCode);
   });
 });
 
 //Respond to POST requests that upload files to uploads/ directory
-app.post('/upload', function(req, res) {
-  if(!req.files) {
+app.post('/upload', function (req, res) {
+  // var svg_image = req.files.svgIMG;
+  if (!req.files) {
+    // console.log(req.files);
     return res.status(400).send('No files were uploaded.');
   }
- 
-  let uploadFile = req.files.uploadFile;
- 
+
+  let uploadFile = req.files.svgIMG;
+
   // Use the mv() method to place the file somewhere on your server
-  uploadFile.mv('uploads/' + uploadFile.name, function(err) {
-    if(err) {
+  uploadFile.mv('uploads/' + uploadFile.name, function (err) {
+    if (err) {
       return res.status(500).send(err);
     }
 
@@ -58,12 +64,12 @@ app.post('/upload', function(req, res) {
 });
 
 //Respond to GET requests for files in the uploads/ directory
-app.get('/uploads/:name', function(req , res){
-  fs.stat('uploads/' + req.params.name, function(err, stat) {
-    if(err == null) {
-      res.sendFile(path.join(__dirname+'/uploads/' + req.params.name));
+app.get('/uploads/:name', function (req, res) {
+  fs.stat('uploads/' + req.params.name, function (err, stat) {
+    if (err == null) {
+      res.sendFile(path.join(__dirname + '/uploads/' + req.params.name));
     } else {
-      console.log('Error in file downloading route: '+err);
+      console.log('Error in file downloading route: ' + err);
       res.send('');
     }
   });
@@ -71,8 +77,10 @@ app.get('/uploads/:name', function(req , res){
 
 //******************** Your code goes here ******************** 
 
+//C 
+
 //Sample endpoint
-app.get('/someendpoint', function(req , res){
+app.get('/someendpoint', function (req, res) {
   let retStr = req.query.name1 + " " + req.query.name2;
   res.send({
     foo: retStr
--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
 // Put all onload AJAX calls here, and event listeners
-$(document).ready(function() {
+$(document).ready(function () {
     // On page-load AJAX Example
     $.ajax({
-        type: 'get',            //Request type
-        dataType: 'json',       //Data type - we will use JSON for almost everything 
-        url: '/someendpoint',   //The server endpoint we are connecting to
+        type: 'get', //Request type
+        dataType: 'json', //Data type - we will use JSON for almost everything 
+        url: '/someendpoint', //The server endpoint we are connecting to
         data: {
             name1: "Value 1",
             name2: "Value 2"
@@ -15,26 +15,30 @@ $(document).ready(function() {
                 so we do not need to parse it on the server.
                 JavaScript really does handle JSONs seamlessly
             */
-            $('#blah').html("On page load, received string '"+data.foo+"' from server");
+            $('#blah').html("On page load, received string '" + data.foo + "' from server");
             //We write the object to the console to show that the request was successful
-            console.log(data); 
+            console.log(data);
 
         },
-        fail: function(error) {
+        fail: function (error) {
             // Non-200 return, do something with error
             $('#blah').html("On page load, received error from server");
-            console.log(error); 
+            console.log(error);
         }
     });
 
     // Event listener form example , we can use this instead explicitly listening for events
     // No redirects if possible
-    $('#someform').submit(function(e){
-        $('#blah').html("Form has data: "+$('#entryBox').val());
+    $('#someform').submit(function (e) {
+        $('#blah').html("Form has data: " + $('#entryBox').val());
         e.preventDefault();
         //Pass data to the Ajax call, so it gets passed to the server
         $.ajax({
             //Create an object for connecting to another waypoint
         });
     });
+
+    // Calculate the file size of the SVG file
+
+    // Need to create function to modify the table so it's dynamically allocating table data when the HTML is done loading
 });
